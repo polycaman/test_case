86b0dae49e8d82adb716e485c5ec359e6392ad4d
polyca-git <faruklittle@yandex.com>
2021-08-27 13:21:52 +0300

    Added ratings

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const DogCard = (props) => {
 
     const defaultSrc = 'https://functions.dogorama-services.com/getStaticAsset?name=park_placeholder';
-
+    const [element,setElement] = useState([]);
     const buttonSetter=()=>{
         props.setButtonSelected(props.data)
     };
+
+    useEffect(() => {
+        fetch(`https://functions.dogorama-services.com/getParks?id=${props.data.id}`)
+        .then(res => res.json())
+        .then(
+          (result)=>{
+              console.log(result.parks[0]);
+            setElement(result.parks[0]);
+          },
+          (error) => {
+            console.log("Error while fetching a single element.");
+          }
+        )
+      }, [])
+
         return (
             <div className="dogCard">
                 <div className="cardImg">
@@ -16,8 +31,9 @@ const DogCard = (props) => {
                     {props.data.name}
                 </div>
                 <div className="cardAdditional">
+                    <div>{parseFloat(element.averageRating).toFixed(2)} out of 5 paws</div>
                     <div>{parseFloat(props.data.distance).toFixed(2)} km away</div>
-                    <div>{props.data.city+", "+props.data.country}</div>
+
                 </div>
                 <div className="cardButtonPart" onClick={buttonSetter}>
                     <span key={props.data.id}>More Info</span>
